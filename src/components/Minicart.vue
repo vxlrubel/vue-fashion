72fde40d0902105426b9260f9f9d44c05b50b952
rubel <vxlrubel@gmail.com>
2024-06-23 00:24:55 +0600

    create minicart component

---
+++ src/components/Minicart.vue
@@ -0,0 +1,43 @@
+<template>
+    <div class="offcanvas offcanvas-end" tabindex="-1" id="minicart" aria-labelledby="shoppingCartTitle">
+      <div class="offcanvas-header px-4">
+        <div class="d-flex" id="shoppingCartTitle">
+          <h4 class="fs-5 m-0">Shopping Cart</h4>
+          <div class="ms-2">items-12</div>
+        </div>
+        <button type="button" class="btn-close shadow-none" data-bs-dismiss="offcanvas" aria-label="Close"></button>
+      </div>
+      <div class="offcanvas-body px-4">
+        <p>Lorem ipsum dolor sit amet.</p>
+        <p>Lorem ipsum dolor sit amet.</p>
+        <p>Lorem ipsum dolor sit amet.</p>
+        <p>Lorem ipsum dolor sit amet.</p>
+        <p>Lorem ipsum dolor sit amet.</p>
+        <p>Lorem ipsum dolor sit amet.</p>
+        <p>Lorem ipsum dolor sit amet.</p>
+        <p>Lorem ipsum dolor sit amet.</p>
+        <p>Lorem ipsum dolor sit amet.</p>
+        <p>Lorem ipsum dolor sit amet.</p>
+        <p>Lorem ipsum dolor sit amet.</p>
+        <p>Lorem ipsum dolor sit amet.</p>
+      </div>
+      <div class="offcanvas-footer px-4 py-3">
+        <div class="row">
+          <div class="col-6">
+            <a href="javascript:void(0)" class="btn btn-sm w-100 btn-primary rounded-0">View Cart</a>
+          </div>
+          <div class="col-6">
+            <a href="javascript:void(0)" class="btn btn-sm w-100 btn-outline-primary rounded-0">Checkout</a>
+          </div>
+        </div>
+      </div>
+    </div>
+</template>
+
+<script setup>
+
+</script>
+
+<style lang="scss" scoped>
+
+</style>
