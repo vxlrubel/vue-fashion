f62651d9d7d7ad5bf1038ec9612e920ec85f77b5
rubel <vxlrubel@gmail.com>
2024-06-23 00:12:52 +0600

    modify some section and include the UserDropdown component

--- src/App.vue
+++ src/App.vue
@@ -3,26 +3,27 @@
   import Iconuser from './components/icons/IconUser.vue';
   import IconCart from './components/icons/IconCart.vue';
   import IconBar from './components/icons/IconBar.vue';
+  import UserDropdown from './components/UserDropdown.vue';
 </script>
 
 <template>
   <header class="clearfix bg-light border-bottom shadow-sm">
     <div class="container">
       <div class="row align-items-center">
-        <div class="col-lg-3">
-          <div class="text-center text-lg-start">
+        <div class="col-6 col-md-12 col-lg-3">
+          <div class="text-md-center text-lg-start">
             <RouterLink to="/" class="text-secondary text-decoration-none fs-4">VueFashion</RouterLink>
           </div>
         </div>
-        <div class="col-md-8 col-lg-6 py-md-2 py-lg-0">
+        <div class="col-md-8 col-lg-6 d-none d-md-block">
           <ul class="list-unstyled d-flex m-0 p-0">
-            <li class="me-1"><RouterLink to="/" class="text-secondary text-decoration-none px-3 py-1">Home</RouterLink></li>
-            <li class="me-1"><RouterLink to="/products" class="text-secondary text-decoration-none px-3 py-1">Products</RouterLink></li>
+            <li class="me-1"><RouterLink to="/" class="text-secondary text-decoration-none px-3 py-3 d-flex align-items-center">Home</RouterLink></li>
+            <li class="me-1"><RouterLink to="/products" class="text-secondary text-decoration-none px-3 py-3 d-flex align-items-center">Products</RouterLink></li>
           </ul>
         </div>
-        <div class="col-md-4 col-lg-3 py-md-2 py-lg-0">
-          <div class="d-flex justify-content-end py-lg-2">
-            <div class="dropdown d-inline-flex d-sm-none">
+        <div class="col-6 col-md-4 col-lg-3 py-1 py-md-0">
+          <div class="d-flex justify-content-end">
+            <div class="dropdown d-inline-flex d-md-none">
               <button type="button" class="btn border-0 shadow-none" data-bs-toggle="collapse" data-bs-target="#collapseExample" aria-expanded="false" aria-controls="collapseExample">
                 <span class="icon"><IconBar /></span>
               </button>
@@ -37,18 +38,8 @@
               
             </div>
             
-            <div class="dropdown d-inline-flex">
-              <button type="button" class="btn border-0" data-bs-toggle="dropdown" aria-expanded="false">
-                <span class="icon">
-                  <Iconuser />
-                </span>
-              </button>
-              <ul class="dropdown-menu">
-                <li><a class="dropdown-item" href="#">Login</a></li>
-                <li><a class="dropdown-item" href="#">Register</a></li>
-                <li><a class="dropdown-item" href="#">Logout</a></li>
-              </ul>
-            </div>
+            <UserDropdown/>
+
           </div>
           
         </div>
@@ -85,34 +76,16 @@
         <p>Lorem ipsum dolor sit amet.</p>
         <p>Lorem ipsum dolor sit amet.</p>
         <p>Lorem ipsum dolor sit amet.</p>
-        <p>Lorem ipsum dolor sit amet.</p>
-        <p>Lorem ipsum dolor sit amet.</p>
-        <p>Lorem ipsum dolor sit amet.</p>
-        <p>Lorem ipsum dolor sit amet.</p>
-        <p>Lorem ipsum dolor sit amet.</p>
-        <p>Lorem ipsum dolor sit amet.</p>
-        <p>Lorem ipsum dolor sit amet.</p>
-        <p>Lorem ipsum dolor sit amet.</p>
-        <p>Lorem ipsum dolor sit amet.</p>
-        <p>Lorem ipsum dolor sit amet.</p>
-        <p>Lorem ipsum dolor sit amet.</p>
-        <p>Lorem ipsum dolor sit amet.</p>
-        <p>Lorem ipsum dolor sit amet.</p>
-        <p>Lorem ipsum dolor sit amet.</p>
-        <p>Lorem ipsum dolor sit amet.</p>
-        <p>Lorem ipsum dolor sit amet.</p>
-        <p>Lorem ipsum dolor sit amet.</p>
-        <p>Lorem ipsum dolor sit amet.</p>
-        <p>Lorem ipsum dolor sit amet.</p>
-        <p>Lorem ipsum dolor sit amet.</p>
-        <p>Lorem ipsum dolor sit amet.</p>
-        <p>Lorem ipsum dolor sit amet.</p>
-        <p>Lorem ipsum dolor sit amet.</p>
-        <p>Lorem ipsum dolor sit amet.</p>
-        <p>Lorem ipsum dolor sit amet.</p>
       </div>
       <div class="offcanvas-footer px-4 py-3">
-        Lorem ipsum dolor, sit amet consectetur
+        <div class="row">
+          <div class="col-6">
+            <a href="javascript:void(0)" class="btn btn-sm w-100 btn-primary rounded-0">View Cart</a>
+          </div>
+          <div class="col-6">
+            <a href="javascript:void(0)" class="btn btn-sm w-100 btn-outline-primary rounded-0">Checkout</a>
+          </div>
+        </div>
       </div>
     </div>
       
