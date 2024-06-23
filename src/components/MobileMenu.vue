0c182ea2fb54b6d73aa729b828f6b5c677db7464
rubel <vxlrubel@gmail.com>
2024-06-24 00:46:11 +0600

    create mobile menu component

---
+++ src/components/MobileMenu.vue
@@ -0,0 +1,17 @@
+<template>
+    <div class="collapse d-md-none" id="collapseExample">
+      <div class="container py-2">
+        <div class="row">
+          <RouterLink to="/" class="d-block text-decoration-none text-body">Home</RouterLink>
+          <RouterLink to="/products" class="d-block text-decoration-none text-body">Products</RouterLink>
+        </div>
+      </div>
+    </div>
+</template>
+
+<script setup>
+</script>
+
+<style lang="scss" scoped>
+
+</style>
